80d63c09596dc8566d67dffc3259c03c4c612594
abhishekkumar0841 <abhishekkumar0841@gmail.com>
2023-12-08 19:33:35 +0530

    successfully implement the functionality for signup or register with user avatar from frontend

--- client/src/Pages/SignupPage.jsx
+++ client/src/Pages/SignupPage.jsx
@@ -6,9 +6,11 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signup } from "../redux/slice/authSlice";
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import { BiUser } from "react-icons/bi";
 
 const SignupPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -19,6 +21,7 @@ const SignupPage = () => {
     userName: "",
     email: "",
     password: "",
+    avatar: "",
   });
 
   function changeHandler(e) {
@@ -29,15 +32,40 @@ const SignupPage = () => {
     });
   }
 
+  const getImage = (e) => {
+    const uploadedImage = e.target.files[0];
+
+    if (uploadedImage) {
+      setInput({
+        ...input,
+        avatar: uploadedImage,
+      });
+
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(uploadedImage);
+      fileReader.addEventListener("load", function () {
+        setPreviewImage(this.result);
+      });
+    }
+  };
+
   async function handleSubmit(e) {
     e.preventDefault();
-    // console.log(input);
-    toast.loading('Wait, your account creating')
+    console.log("Check input-->", input);
+    toast.loading("Wait, your account creating");
+
+    const formData = new FormData();
+    formData.append("firstName", input.firstName);
+    formData.append("lastName", input.lastName);
+    formData.append("userName", input.userName);
+    formData.append("email", input.email);
+    formData.append("password", input.password);
+    formData.append("avatar", input.avatar);
+
     try {
-      const response = await axiosInstance.post("/user/signup", input);
-      // console.log('RESPONSE OF SIGNUP->', response)
+      const response = await axiosInstance.post("/user/signup", formData);
       if (response?.data?.success) {
-        toast.dismiss()
+        toast.dismiss();
         toast.success("Registration successful");
         setInput({
           firstName: "",
@@ -45,13 +73,17 @@ const SignupPage = () => {
           userName: "",
           email: "",
           password: "",
+          avatar: "",
         });
+
+        //removing the avatar and only send serializable data in redux store
+        const userWithoutAvatar = { ...input };
+        delete userWithoutAvatar.avatar;
+        dispatch(signup(userWithoutAvatar));
         navigate("/login");
-        dispatch(signup(input));
       }
     } catch (error) {
-      // console.log(error)
-      toast.dismiss()
+      toast.dismiss();
       toast.error(error?.response?.data?.message);
     }
   }
@@ -68,6 +100,26 @@ const SignupPage = () => {
           className=" w-96 shadow-[0_0_10px_gray] px-4 py-8 flex flex-col gap-4 rounded-md"
         >
           <h1 className=" text-4xl font-bold text-center">Signup Form</h1>
+          <div className=" flex flex-col items-center">
+            <label htmlFor="avatar" className="cursor-pointer">
+              {previewImage ? (
+                <img
+                  src={previewImage}
+                  className="w-24 h-24 rounded-full m-auto"
+                />
+              ) : (
+                <BiUser className="w-24 h-24 rounded-full m-auto" />
+              )}
+            </label>
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              onChange={getImage}
+              accept=".jpg, .jpeg, .png, .svg"
+              className="hidden"
+            />
+          </div>
           <div className="">
             <label htmlFor="firstName" className=" block">
               First Name:
