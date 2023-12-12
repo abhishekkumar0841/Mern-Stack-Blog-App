d2283b89e010925c836a7a1abbfac2e58954171d
abhishekkumar0841 <abhishekkumar0841@gmail.com>
2023-12-12 23:32:13 +0530

    showing blogImage and author avatar on the blogTemplatePage.jsx

--- client/src/Components/BlogComponents/BlogTemplate.jsx
+++ client/src/Components/BlogComponents/BlogTemplate.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import { useSelector } from "react-redux";
 import BlogCardFooter from "./BlogCardFooter";
 import { useNavigate } from "react-router-dom";
-import {BsFillArrowLeftCircleFill} from 'react-icons/bs'
+import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import BlogLogo from "../../assets/images/blogLogo.png";
 
 const BlogTemplate = () => {
   const { blogById } = useSelector((state) => state.blog);
-  // console.log("BLOG BY ID-->", blogById);
+  const authorAvatar = blogById?.author?.avatar?.secure_url;
+  const blogImage = blogById?.blogImage;
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className=" min-h-[90vh] px-2 flex items-center justify-center ">
       <div className="w-full max-w-[800px] mx-auto py-6 dark:text-gray-200 text-gray-900 transition-all duration-300 ease-in-out shadow-[0_0_10px_gray] my-10 ">
-        
         <div className=" relative px-5 py-3">
-        <div onClick={()=>navigate(-1)}>
-            <BsFillArrowLeftCircleFill className=" text-4xl cursor-pointer"/>
-        </div>
+          <div onClick={() => navigate(-1)}>
+            <BsFillArrowLeftCircleFill className=" text-4xl cursor-pointer" />
+          </div>
           <div className=" text-center">
             <h1 className=" text-3xl font-bold capitalize  w-1/2 mx-auto">
               {blogById?.title}
@@ -30,17 +31,17 @@ const BlogTemplate = () => {
             </p>
           </div>
           <div className=" absolute right-5 top-4 ">
-            <img
-              src={blogById?.postedBy?.avatar?.secure_url}
-              alt=""
-              width={100}
-              className=" rounded-md"
-            />
+            <img src={authorAvatar} alt="" width={80} className=" rounded-md" />
           </div>
         </div>
         {/* description & content section */}
         <div className=" px-5 py-3 text-center space-y-4 text-xl">
-          <p className="font-bold">{blogById?.description}</p>
+          <p className="font-bold capitalize">{blogById?.description}</p>
+          <img
+            src={blogImage ? blogImage : BlogLogo}
+            alt="Blog Image"
+            className=" w-full h-72 object-fill rounded-xl"
+          />
           <p dangerouslySetInnerHTML={{ __html: blogById?.blogContent }} />
         </div>
 
